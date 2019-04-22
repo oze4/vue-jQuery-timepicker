dea27534338a550ade176f9bbd3d4a2909d6df8a
oze4 <matthewpoestreich@gmail.com>
2019-04-22 15:01:00 -0500

    cleaned up assets, rebuild

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ window.$ = window.jQuery = require('jquery');
 require('jquery-ui/ui/widgets/datepicker.js')
 require('jquery-ui/ui/widgets/slider.js')
 require('jquery-ui-timepicker-addon')
-import 'jquery-ui/themes/base/all.css'
+import 'jquery-ui/themes/base/datepicker.css'
+import 'jquery-ui/themes/base/slider.css'
+import 'jquery-ui/themes/base/theme.css'
 import 'jquery-ui-timepicker-addon/dist/jquery-ui-timepicker-addon.min.css'
 
 Vue.config.productionTip = false
